c43bc8a6677c8c4610adc87f4bc4ed0e91eac6fd
AxelVanHout <vanhoutaxel.avhmcpe@gmail.com>
2023-10-29 15:59:27 +0100

    slide-update

--- script.js
+++ script.js
@@ -1,49 +1,15 @@
-let slide = document.querySelectorAll('.slide');
-var current = 0;
-var intervalId;
+const images = [
+    "url('images/1.jpg')",
+    "url('images/2.jpg')",
+    "url('images/3.jpg')"
+];
 
-function cls() {
-    for (let i = 0; i < slide.length; i++) {
-        slide[i].style.opacity = 0;
-        slide[i].style.display = 'none';
-    }
+let currentImageIndex = 0;
+
+function changeBackground() {
+    document.querySelector(".slide-background").style.backgroundImage = images[currentImageIndex];
+    currentImageIndex = (currentImageIndex + 1) % images.length;
 }
 
-function next() {
-    cls();
-    if (current === slide.length - 1) current = -1;
-    current++;
-
-    slide[current].style.display = 'block';
-    slide[current].offsetHeight;
-    slide[current].style.opacity = 1;
-}
-
-function prev() {
-    cls();
-    if (current === 0) current = slide.length;
-    current--;
-
-    slide[current].style.display = 'block';
-    slide[current].offsetHeight;
-    slide[current].style.opacity = 1;
-}
-
-function start() {
-    cls();
-    slide[current].style.display = 'block';
-    slide[current].style.opacity = 1;
-}
-
-function startAutoSlide() {
-    intervalId = setInterval(function () {
-        next();
-    }, 5000);
-}
-
-function stopAutoSlide() {
-    clearInterval(intervalId);
-}
-
-start();
-startAutoSlide();
+changeBackground();
+setInterval(changeBackground, 4000);
